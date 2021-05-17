112c23427ddad951322d32373f56717e702eeb33
Jesse Stewart <jessestewart@Jesses-MBP.lan>
2021-05-17 14:41:45 -0500

    Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
+// import Hello from './components/Hello'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick'
+// import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World.
         </p>
@@ -19,15 +20,16 @@ function App() {
         <Welcome name="Bruce" heroName="Batman" />
         <FunctionClick />
         <Counter />
-        <ClassClick />
-        <a
+        <ClassClick />  */}
+        <EventBind />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
